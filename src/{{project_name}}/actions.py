bf837cd8b685c9845e60c4c6b878542f5ca585fd
Chen Buskilla <chen@buskilla.com>
2023-11-28 14:25:33 +0200

    add basic template

---
+++ src/{{project_name}}/actions.py
@@ -0,0 +1,21 @@
+
+
+import asyncio
+from missioncontrolbot import MissionControl
+
+def add_user_to_newsletter(email:str):
+    """
+    Add a user to the newsletter.
+    """
+    return {"message": f"User {email} added to newsletter. 2"}
+
+async def launch_rocket():
+    """
+    Launch a rocket.
+    """
+    for i in range(3):
+        print(f"Rocket launching in {3-i}...")
+        await asyncio.sleep(1)
+    return {"message": "Rocket launched!"}
+
+actions = MissionControl([launch_rocket, add_user_to_newsletter])
